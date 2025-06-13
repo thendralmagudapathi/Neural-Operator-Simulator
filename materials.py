9de0353f2dc76dabc59e32784445ce3bbf7af292
Thendral Magudapathi <121350906+thendralmagudapathi@users.noreply.github.com>
2025-06-14 00:58:25 +0530

    Add files via upload

---
+++ materials.py
@@ -0,0 +1,6 @@
+# Default properties (could be extended later)
+MATERIAL_PROPERTIES = {
+    "Steel": {"k": 43, "rho": 7850, "c": 470},
+    "Aluminum": {"k": 205, "rho": 2700, "c": 900},
+    "Iron": {"k": 80, "rho": 7874, "c": 450},
+}
